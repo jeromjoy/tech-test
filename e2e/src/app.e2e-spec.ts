54a97432df1add5588bfef286e37468b69d421f6
Jerom Joy Vattakeril <jeromjoy@users.noreply.github.com>
2021-02-07 22:33:49 +0000

    updated e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('tech-test app is running!');
+    expect(await page.getTitleText()).toEqual('Tech Test');
   });
 
   afterEach(async () => {
